8eb9a5680b66356f488093966cf6f9e0044b2782
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-01-05 12:22:40 +0000

    First push - Experiments with LPC sprites and lighting 2d sprites in 3d space, using a shader graph built shader

---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    Animator        anim;
+    SpriteRenderer  spriteRenderer;
+    Vector2         lastMoveDir = new Vector2(0.0f, -1.0f);
+    Material        spriteMaterial;
+    Coroutine       flashCR;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteMaterial = spriteRenderer.material;
+    }
+
+    void Update()
+    {
+        Vector2 mv = Vector2.zero;
+        mv.x = Input.GetAxis("Horizontal");
+        mv.y = Input.GetAxis("Vertical");
+
+        if (mv.magnitude > 0.1f)
+        {
+            lastMoveDir = mv.normalized;
+        }
+
+        anim.SetFloat("DirX", lastMoveDir.x);
+        anim.SetFloat("DirY", lastMoveDir.y);
+
+        anim.SetFloat("Speed", mv.magnitude);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Slash");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            anim.SetTrigger("Thrust");
+        }
+
+        anim.SetBool("Shoot", Input.GetButton("Fire3"));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (flashCR != null)
+            {
+                StopCoroutine(flashCR);
+            }
+            flashCR = StartCoroutine(FlashCR());
+        }
+    }
+
+    IEnumerator FlashCR()
+    {
+        Color c = Color.red;
+
+        while (c.a > 0.0f)
+        {
+            spriteMaterial.SetColor("_Color", c);
+
+            c.a -= Time.deltaTime * 2.0f;
+
+            yield return null;
+        }
+
+        c.a = 0;
+        spriteMaterial.SetColor("_Color", c);
+
+        flashCR = null;
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# GreedCave
+
+This is a game done for the 4th Game Design Club challenge (Arcade) at the Universidade Lusófona Game Development degree.
+
+## Story
+
+Comming soon...
+
+## Tech stuff
+
+Comming soon...
+
+## Credits
+
+* Code, art, game design done by Diogo de Andrade
+* Character sprites generated using [Universal LPC Spritesheet Character Generator]
+  * Check AUTHORS.TXT file for attribution as far as I could find it
+* LPC import code based on bitcula's [Universal LPC Spritesheet Unity Importer]
+
+## Licenses
+
+All code in this repo is made available through the [GPLv3] license.
+The text and all the other files are made available through the 
+[CC BY-NC-SA 4.0] license.
+LPC art made available through the [GPLv3] and [CC-BY-SA 3.0.] licenses.
+
+## Metadata
+
+* Autor: [Diogo Andrade][]
+
+[Diogo Andrade]:https://github.com/DiogoDeAndrade
+[GPLv3]:https://www.gnu.org/licenses/gpl-3.0.en.html
+[CC-BY-SA 3.0.]:http://creativecommons.org/licenses/by-sa/3.0/
+[CC BY-NC-SA 4.0]:https://creativecommons.org/licenses/by-nc-sa/4.0/
+[Bfxr]:https://www.bfxr.net/
+[Universal LPC Spritesheet Character Generator]:https://sanderfrenken.github.io/
+[Universal LPC Spritesheet Unity Importer]:https://github.com/bitcula/Universal-LPC-Spritesheet-Unity-Importer
